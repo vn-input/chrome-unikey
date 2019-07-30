231cb47944ba7119042dfe23db43eff84641e51e
Lê Quốc Tuấn <mr.lequoctuan@gmail.com>
2019-07-30 13:59:30 +0700

    show options page on installed/update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 var ime_api = chrome.input.ime;
 
+let compareVer = function(v1, v2) {
+	let a1 = v1.split('.').map(i=>parseInt(i, 10))
+	let a2 = v2.split('.').map(i=>parseInt(i, 10))
+
+	let len = Math.max(a1.length, a2.length)
+	for (let i = 0; i < len; i++) {
+		let x1 = a1[i]||0
+		let x2 = a2[i]||0
+		if (x1 > x2) {
+			return 1
+		} else if (x1 < x2) {
+			return -1
+		}
+	}
+	return 0
+}
+
 if (ime_api.onActivate && ime_api.onFocus && ime_api.onBlur && ime_api.onKeyEvent && ime_api.onMenuItemActivated) {
 	var crxuk = require('./crxuk');
 
@@ -26,6 +43,13 @@ if (ime_api.onActivate && ime_api.onFocus && ime_api.onBlur && ime_api.onKeyEven
 			ime_api.onKeyEvent.removeListener(onKeyEvent);
 			ime_api.onMenuItemActivated.removeListener(onMenuItemActivated);
 			ime_api.onCandidateClicked.removeListener(onCandidateClicked);
+		} else {
+			chrome.runtime.onInstalled.addListener(function(detail) {
+				if (detail.reason == "install" ||
+					(detail.reason == "update" && compareVer(detail.previousVersion, "0.6.0.9999") <= 0 )
+					)
+				chrome.tabs.create({url: "options.html"})
+			})
 		}
 	});
 }
